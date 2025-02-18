ae5e401b147ff7ff9870df7494496b77fe8f76b3
ali.ms.shishani <ali.ms.shishani@gmail.com>
2025-02-18 17:49:43 +1100

    create back-end service layer unit test

---
+++ InterviewProjectTemplate.Services.Tests/MooRatingServicedTests/GetMoodRatingOptionsTest.cs
@@ -0,0 +1,55 @@
+﻿using AutoBogus;
+using FluentAssertions;
+using InterviewProjectTemplate.Data;
+using InterviewProjectTemplate.Repositories;
+using InterviewProjectTemplate.Services.Mood;
+using Moq;
+using Moq.AutoMock;
+using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProjectTemplate.Services.Tests.MooRatingServicedTests
+{
+    [TestClass]
+    public class GetMoodRatingOptionsTest
+    {
+        private AutoMocker _autoMocker;
+        private IAutoFaker _faker;
+        private Mock<MoodRatingDbContext> _mockMoodRatingDbContext;
+        private Mock<IMoodRatingRecordRepository> _mockMoodRatingRecordRepository;
+        private MoodRatingService _mockMoodRatingService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _autoMocker = new AutoMocker();
+            _faker = AutoFaker.Create(builder => builder.WithRecursiveDepth(1).WithTreeDepth(2).WithRepeatCount(1));
+
+            _mockMoodRatingDbContext = _autoMocker.GetMock<MoodRatingDbContext>();
+            _mockMoodRatingService = _autoMocker.CreateInstance<MoodRatingService>();
+        }
+
+        [TestMethod]
+        public async Task GetMoodRatingOptions_Returns_4_options()
+        {
+            var (result, errors) = await _mockMoodRatingService.GetMoodRatingOptions();
+
+            result.MoodRatingOptions.Count.Should().Be(4);
+        }
+
+        [TestMethod]
+        public async Task GetMoodRatingOptions_Returns_No_Duplicate_Options()
+        {
+            var (result, errors) = await _mockMoodRatingService.GetMoodRatingOptions();
+
+            var containsDuplicates = result.MoodRatingOptions
+                                    .Any(s => result.MoodRatingOptions.Count(x => x.Code == s.Code) > 1);
+
+            containsDuplicates.Should().Be(false);
+        }
+    }
+}
---
+++ InterviewProjectTemplate.Services.Tests/MooRatingServicedTests/RecordMoodRatingTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoBogus;
+using InterviewProjectTemplate.Data;
+using InterviewProjectTemplate.Repositories;
+using InterviewProjectTemplate.Services.Mood;
+using Moq;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProjectTemplate.Services.Tests.MooRatingServicedTests
+{
+    [TestClass]
+    public class RecordMoodRatingTest
+    {
+        private AutoMocker _autoMocker;
+        private IAutoFaker _faker;
+        private Mock<MoodRatingDbContext> _mockMoodRatingDbContext;
+        private Mock<IMoodRatingRecordRepository> _mockMoodRatingRecordRepository;
+        private MoodRatingService _mockMoodRatingService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _autoMocker = new AutoMocker();
+            _faker = AutoFaker.Create(builder => builder.WithRecursiveDepth(1).WithTreeDepth(2).WithRepeatCount(1));
+
+            _mockMoodRatingDbContext = _autoMocker.GetMock<MoodRatingDbContext>();
+            _mockMoodRatingService = _autoMocker.CreateInstance<MoodRatingService>();
+        }
+
+        [TestMethod]
+        public async Task RecordMoodRating_Should_Record_Successfully()
+        {
+            // TODO: implement test logic
+        }
+
+        [TestMethod]
+        public async Task RecordMoodRating_Should_Not_Record_Duplicates()
+        {
+            // TODO: implement test logic
+        }
+    }
+}
